074e06fa9ec1fd2338fbfac4abe07f6403fce39e
Greg Santos <gregsantos@gmail.com>
2025-10-25 12:03:01 -0700

    add db url to drizzle config

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -1,4 +1,10 @@
-import { defineConfig } from "drizzle-kit"
+import {defineConfig} from "drizzle-kit"
+import dotenv from "dotenv"
+dotenv.config({path: ".env.local"})
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set")
+}
 
 export default defineConfig({
   schema: "./lib/server/db/schema/index.ts",
